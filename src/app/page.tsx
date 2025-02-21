b673d6eb6c3d1b77eefc92ed86c3f6d3ea8238f9
unknown <davidedde7@gmail.com>
2025-02-21 16:51:51 +0100

    Reuseable Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import UserBlock from './Components/UserBlock'
+
 
 export default function page() {
   return (
-    <div><UserBlock balance='0.10000 STRK' profile="/profile.png" />
+    <div>
     
     </div>
   )
