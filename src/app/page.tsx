6130de72582d62bc54e4f6ee6bb84ec845793fa8
Ezeh20 <ranter1570@gmail.com>
2025-02-21 20:56:18 +0100

    fix: fixed the root page error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-
-
 export default function Home() {
-  return (
-  <div>
-  </div>
-  
-   );
-
-
-
-export default function Home() {
-  return (
-    <div>
-      
-    </div>
-  );
-
+	return <div></div>;
 }
