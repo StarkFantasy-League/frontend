981704d3b028f07f3929a974978103d4622d2d0d
tali-creator <talinanzing111@gmail.com>
2025-02-24 03:26:19 +0100

    {commit_message}

--- src/app/leagues/components/premierLeague.tsx
+++ src/app/leagues/components/premierLeague.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { useState } from "react";
 
-const MxLeaguePage = () => {
+const PremierLeague = () => {
   const [user, setUser] = useState({
     isConnected: true,
     walletAddress: "0x1234abcd5678efgh",
@@ -20,14 +20,18 @@ const MxLeaguePage = () => {
       />
       <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-[#221D2F]/[0.82] via-[#1F1B2C]/[0.31] to-[#1D1829]/[0] z-10"></div>
       <div className="relative z-20 flex flex-col items-center leading-[70px] text-center p-4 mt-10 ">
-        <h2 className="text-lg md:text-2xl tracking-[0.2em]  uppercase">Welcome to</h2>
-        <h1 className="text-[40px] sm:text-[70px] md:text-[95px] lg:[120px] font-black mt-2">Premier League</h1>
-        <h3 className="text-xl sm:text-2xl font-medium tracking-[1.2em] md:text-3xl md:tracking-[2.2em] lg:text-4xl lg:tracking-[1.9em] md:mt-5 sm:tracking-[2em] mt-2">
-          Tournament
-        </h3>
-        {user.isConnected && (
+     
+        {user.isConnected ? (
+          <>
+            <h2 className="text-lg md:text-2xl tracking-[0.2em]  uppercase">Welcome to</h2>
+            <h1 className="text-[40px] sm:text-[70px] md:text-[95px] lg:[120px] font-black mt-2">Premier League</h1>
+            <h3 className="text-xl sm:text-2xl font-medium tracking-[1.2em] md:text-3xl md:tracking-[2.2em] lg:text-4xl lg:tracking-[1.9em] md:mt-5 sm:tracking-[2em] mt-2">
+              Tournament
+            </h3>
+          </>
+        ) :(
           <p className="mt-4 text-sm md:text-base">
-            Connected as: {user.walletAddress}
+            Connect your wallet to join the tournament
           </p>
         )}
       </div>
@@ -35,4 +39,4 @@ const MxLeaguePage = () => {
   );
 };
 
-export default MxLeaguePage;
+export default PremierLeague;
